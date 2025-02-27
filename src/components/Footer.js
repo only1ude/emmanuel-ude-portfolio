e5fbc3e41386b6688f5545b3fb7ef8988b191c3e
emmanuel ude <only1ude@gmail.com>
2025-02-27 05:27:55 -0600

    commit to main

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import '../styles/Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <div className="footer-left">
+                    <p>OIU</p>
+                    <ul className="footer-links">
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#skills">Skills</a></li>
+                        <li><a href="#portfolio">Portfolio</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                    <div className="social-icons">
+                        <img src="social media link here" alt="social meida icon" />
+                        <img src="social media link here" alt="social meida icon" />
+                        <img src="social media link here" alt="social meida icon" />
+                        <img src="social media link here" alt="social meida icon" />
+                        <span>© 2021 OIU</span> 
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
