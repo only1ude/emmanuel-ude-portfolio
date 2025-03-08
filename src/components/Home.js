1f524ea3198bbac264b419b26521b9c069a2ebfa
emmanuel ude <only1ude@gmail.com>
2025-03-08 12:48:13 -0600

    commit to main

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 import '../styles/Home.css';
-import personImage from "../assets/person.jpg"; //correct the path to the image
+import personImage from '..assets/assets/person.jpg'; // If person.jpg is in the parent directory 
 
 function Home() {
     return (
         <section id="home" className="home">
            <h1>HELLO</h1> 
-           <img src="person.jpg" alt="OIU" className="person-image" />
+           <img src="/assets/person.jpg" alt="OIU" className="person-image" />
            <p>Scroll down</p>
          </section>
     ); 
